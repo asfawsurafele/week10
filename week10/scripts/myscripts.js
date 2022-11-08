3366df96f24247d41a585362641e2649cab7d414
Surafele Asfaw <surafeleasfaw@Surafeles-MacBook-Air.local>
2022-11-08 14:48:22 -0800

    while and for loop use

---
+++ week10/scripts/myscripts.js
@@ -0,0 +1,14 @@
+for (let i = 1; i < 11; i++)
+{
+    let num = 2;
+    let ans = num ** i;
+    console.log(ans);
+}
+let exp = 1;
+while (exp <= 10)
+{
+    let num = 2;
+    let ans = num ** exp;
+    console.log(ans);
+    exp += 1
+}

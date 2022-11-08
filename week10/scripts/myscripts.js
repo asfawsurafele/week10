1f555a84bef7826c0af405226633de03cdafda0a
Surafele Asfaw <surafeleasfaw@Surafeles-MacBook-Air.local>
2022-11-08 15:16:24 -0800

    user prompts

--- week10/scripts/myscripts.js
+++ week10/scripts/myscripts.js
@@ -1,14 +1,22 @@
 for (let i = 1; i < 11; i++)
 {
-    let num = 2;
-    let ans = num ** i;
-    console.log(ans);
+    console.log(2 ** i);
 }
 let exp = 1;
 while (exp <= 10)
 {
-    let num = 2;
-    let ans = num ** exp;
-    console.log(ans);
+    console.log(2 ** exp);
     exp += 1
 }
+
+
+let user = prompt ("What is your name?:");
+console.log(Number(user));
+
+
+
+let input;
+do {
+    user = prompt ("What is your name?:");
+}  while (!isNaN(Number(user)));
+alert(`Hello ${user}`);

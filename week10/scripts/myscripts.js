f32d0166b02848e4a5aefb275e757db8470d0c68
Surafele Asfaw <surafeleasfaw@Surafeles-MacBook-Air.local>
2022-11-08 16:01:22 -0800

    array editing

--- week10/scripts/myscripts.js
+++ week10/scripts/myscripts.js
@@ -1,22 +1,32 @@
-for (let i = 1; i < 11; i++)
-{
-    console.log(2 ** i);
-}
-let exp = 1;
-while (exp <= 10)
-{
-    console.log(2 ** exp);
-    exp += 1
-}
+// for (let i = 1; i < 11; i++)
+// {
+//     console.log(2 ** i);
+// }
+// let exp = 1;
+// while (exp <= 10)
+// {
+//     console.log(2 ** exp);
+//     exp += 1
+// }
 
 
-let user = prompt ("What is your name?:");
-console.log(Number(user));
+// let user = prompt ("What is your name?:");
+// console.log(Number(user));
 
 
 
-let input;
-do {
-    user = prompt ("What is your name?:");
-}  while (!isNaN(Number(user)));
-alert(`Hello ${user}`);
+// let input;
+// do {
+//     user = prompt ("What is your name?:");
+// }  while (!isNaN(Number(user)));
+// alert(`Hello ${user}`);
+
+let fruits =["Apple", "Banana"];
+fruits.push("Orange"); // add item to end of array
+console.log(fruits)
+
+fruits.pop("Orange"); // removes item from arrray
+
+fruits.unshift("Mango") //adds item to beginning of array
+
+fruits.shift() //removes first item at beginning of array and returns value
